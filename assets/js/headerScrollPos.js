6a75bd05eca76f05e2477f2715e813688b4b1187
Babken <76743515+Babken2005@users.noreply.github.com>
2021-11-06 23:14:10 +0400

    Update headerScrollPos.js

--- assets/js/headerScrollPos.js
+++ assets/js/headerScrollPos.js
@@ -1,4 +1,8 @@
 const header = document.querySelector('.homeHeader');
+const open = document.querySelector('.ri-menu-3-line');
+const close = document.querySelector('.ri-close-fill');
+const menu = document.querySelector('.mobileMenu');
+
 
 window.addEventListener('scroll', () => {
   let scrollPos = window.scrollY;
@@ -9,4 +13,12 @@ window.addEventListener('scroll', () => {
   else {
     header.classList.remove('headerScrolled');
   }
-})
+});
+
+open.addEventListener('click', () => {
+  menu.style.transform="translateX(0%)"
+});
+
+close.addEventListener('click', () => {
+  menu.style.transform="translateX(100%)"
+});
